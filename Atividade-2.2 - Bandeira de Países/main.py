6ba989149a9b20d2c7b04e136a16ef72dd3ee0c8
vitorllr <vitorlopeslrocha@gmail.com>
2026-03-23 12:44:46 -0300

    funcao para desenho de bandeira na horizontal

--- Atividade-2.2 - Bandeira de Países/main.py
+++ Atividade-2.2 - Bandeira de Países/main.py
@@ -11,6 +11,9 @@ largura_faixa = 75
 altura_faixa = 150
 
 
+largura_faixa_horizontal = 210
+altura_faixa_horizontal = 50
+
 
 def desenha_bandeira_simples(cores:list,largura:float,altura:float,nome_pais:str):
 
@@ -49,9 +52,43 @@ def desenha_bandeira_simples(cores:list,largura:float,altura:float,nome_pais:str
     
     turtle.hideturtle()
 
+def desenha_bandeira_simples_horizontal(cores:list, largura:float, altura:float, nome_pais:str):
+    # Posicionamento inicial
+    turtle.penup()
+    turtle.goto(-largura/2, 100) # Centraliza melhor no eixo X
+    turtle.pendown()
+    turtle.setheading(0) # Garante que está olhando para a direita
+    turtle.color("black")
+
+    start_x, start_y = turtle.pos()
+
+    for i in cores:
+        turtle.fillcolor(i)
+        turtle.begin_fill()
+        # Desenha o retângulo 
+        for _ in range(2):
+            turtle.forward(largura)
+            turtle.right(90)
+            turtle.forward(altura)
+            turtle.right(90)
+        turtle.end_fill()
+
+        # Move pra baixo para a próxima faixa
+        turtle.penup()
+        turtle.right(90)
+        turtle.forward(altura)
+        turtle.left(90)
+        turtle.pendown()
+
+    # 3. Nome do país (acima da primeira faixa)
+    turtle.penup()
+    turtle.goto(0, start_y + 10)
+    turtle.write(nome_pais, align="center", font=("Arial", 16, "bold"))
+    turtle.hideturtle()
+
 
 #Bandeira Costa do Marfim - 1
-
+colors = ["orange","white","green"]
 desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Costa do Marfim')
 sleep(1)
 turtle.clear()
@@ -105,17 +142,33 @@ desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Romênia')
 sleep(1)
 turtle.clear()
 
-#Bandeira Romênia - 9
+#Bandeira Alemanha - 9
 
-colors = ["blue","yellow","red"]
-desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Romênia')
+colors = ["black","red","gold"]
+desenha_bandeira_simples_horizontal(colors,largura_faixa,altura_faixa,'Alemanha')
 sleep(1)
 turtle.clear()
 
-#Bandeira Romênia - 10
+#Bandeira Russia - 10
 
-colors = ["blue","yellow","red"]
-desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Romênia')
+colors = ["white","blue","red"]
+desenha_bandeira_simples_horizontal(colors,largura_faixa_horizontal,altura_faixa_horizontal,'Russia')
+sleep(1)
+turtle.clear()
+
+#Bandeira Países Baixos - 11
+
+colors = ["red","white","blue"]
+desenha_bandeira_simples_horizontal(colors,largura_faixa_horizontal,altura_faixa_horizontal,'Países Baixos')
+sleep(1)
+turtle.clear()
+
+
+
+#Bandeira Bulgária - 12
+
+colors = ["red","white","blue"]
+desenha_bandeira_simples_horizontal(colors,largura_faixa_horizontal,altura_faixa_horizontal,'Bulgária')
 sleep(1)
 turtle.clear()
 
