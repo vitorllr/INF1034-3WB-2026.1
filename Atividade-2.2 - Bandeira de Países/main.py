d7a80595d6753286c64798cd38d09f57323c56f6
vitorllr <vitorlopeslrocha@gmail.com>
2026-03-23 12:29:47 -0300

    Listras Verticais

--- Atividade-2.2 - Bandeira de Países/main.py
+++ Atividade-2.2 - Bandeira de Países/main.py
@@ -6,12 +6,14 @@ turtle = Turtle()
 turtle.speed(5)
 
 
-#Bandeira Costa do Marfim
 colors = ["orange","white","green"]
 largura_faixa = 75
 altura_faixa = 150
 
-def desenha_bandeira_simples(cores:list,largura:float,altura:float):
+
+
+def desenha_bandeira_simples(cores:list,largura:float,altura:float,nome_pais:str):
+
 
     # Posicionamento inicial (para não começar no meio da tela)
     turtle.penup()
@@ -34,29 +36,92 @@ def desenha_bandeira_simples(cores:list,largura:float,altura:float):
         turtle.forward(largura)
         turtle.pendown()
 
+    # 3. Coloca o nome do país acima da bandeira
+    
+    centro_x = largura/2
+    posicao_y_texto = altura + 20
+    
+    turtle.penup()
+    turtle.goto(centro_x, posicao_y_texto)
+    turtle.color("black")
+    
+    turtle.write(nome_pais, align="center", font=("Arial", 16, "bold"))
+    
+    turtle.hideturtle()
 
 
-desenha_bandeira_simples(colors,largura_faixa,altura_faixa)
-sleep(3)
+#Bandeira Costa do Marfim - 1
 
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Costa do Marfim')
+sleep(1)
 turtle.clear()
 
-#Bandeira França
+#Bandeira França - 2
 colors = ["blue","white","red"]
-desenha_bandeira_simples(colors,largura_faixa,altura_faixa)
-sleep(3)
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'França')
+sleep(1)
+turtle.clear()
 
 
-#Bandeira Itália 
+#Bandeira Itália - 3
 colors = ["green","white","red"]
-desenha_bandeira_simples(colors,largura_faixa,altura_faixa)
-sleep(3)
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa, "Itália")
+sleep(1)
+turtle.clear()
 
-#Bandeira Irlanda
+#Bandeira Irlanda - 4
 
 colors = ["green","white","orange"]
-desenha_bandeira_simples(colors,largura_faixa,altura_faixa)
-sleep(3)
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Irlanda')
+sleep(1)
+turtle.clear()
+
+#Bandeira Bélgica - 5
+
+colors = ["black","yellow","red"]
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Bélgica')
+sleep(1)
+turtle.clear()
 
 
-done()
+#Bandeira Peru - 6
+
+colors = ["red","white","red"]
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Peru')
+sleep(1)
+turtle.clear()
+
+#Bandeira Nigéria - 7
+
+colors = ["green","white","green"]
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Nigéria')
+sleep(1)
+turtle.clear()
+
+#Bandeira Romênia - 8
+
+colors = ["blue","yellow","red"]
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Romênia')
+sleep(1)
+turtle.clear()
+
+#Bandeira Romênia - 9
+
+colors = ["blue","yellow","red"]
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Romênia')
+sleep(1)
+turtle.clear()
+
+#Bandeira Romênia - 10
+
+colors = ["blue","yellow","red"]
+desenha_bandeira_simples(colors,largura_faixa,altura_faixa,'Romênia')
+sleep(1)
+turtle.clear()
+
+
+
+
+
+
+mainloop()
