059d20d0d373c236e75e6681e61dfb58576c03c6
vitor-rocha-quality <vitorlopeslrocha@gmail.com>
2026-03-25 22:21:12 -0300

    quadrantes definidos corretamente

--- Atividade-3.1- Funcoes-de-formas-geometricas/main.py
+++ Atividade-3.1- Funcoes-de-formas-geometricas/main.py
@@ -24,18 +24,14 @@ def desenhar_plano_cartesiano():
     t.stamp()
 
     t.hideturtle()
-    #t.penup() # ou pu()
-    #t.goto(200,250)
-    #t.pendown() # ou pd()
+   
+def desenhar_quadrado(fill_color: str, lado:int):
 
-
-def desenhar_quadrado(x, y, fill_color: str, lado:int):
-
-    x = rand_x - lado
-    y = rand_y -lado
+    rand_x = random.randint(0, 300 - lado)                                                                  
+    rand_y = random.randint(lado, 300)
 
     t.penup()
-    t.goto(x,y)
+    t.goto(rand_x,rand_y)
     t.setheading(0)
     t.pendown()
 
@@ -50,7 +46,6 @@ def desenhar_quadrado(x, y, fill_color: str, lado:int):
 
     t.hideturtle()
 
-
 def desenhar_triangulo_equilatero(fill_color: str, lado:int):
     altura = lado * math.sqrt(3)/2
     rand_x = random.randint(0, int(300 - lado))
@@ -61,9 +56,8 @@ def desenhar_triangulo_equilatero(fill_color: str, lado:int):
     t.pendown()
 
 
-    t.color(fill_color) #Cor da borda
-    t.begin_fill() # Usa a cor que a T estiver na hora se nao definido o preenchimento
-    t.fillcolor(color)
+    t.begin_fill()
+    t.fillcolor(fill_color)
 
     for _ in range(3): 
         t.fd(lado)
@@ -74,8 +68,8 @@ def desenhar_triangulo_equilatero(fill_color: str, lado:int):
 def desenhar_octogono(fill_color: str, lado:int):
     altura = int(lado*(1 + math.sqrt(2)))
 
-    rand_x = random.randint(-300,altura)
-    rand_y = random.randint(-300,int(300-altura))
+    rand_x = random.randint(-300, -altura)
+    rand_y = random.randint(-300, -altura)
 
     t.penup() # ou pu()
     t.goto(rand_x, rand_y)
@@ -94,8 +88,11 @@ def desenhar_octogono(fill_color: str, lado:int):
 def desenhar_trapezio(fill_color: str, lado_menor:int, lado_maior:int, lado_nao_paralelo:int):
 
 
-    rand_x = random.randint(-300,lado_maior)
-    rand_y = random.randint(1,(300-altura))
+    angulo_rad = math.radians(50)                                                                      
+    altura = int(lado_nao_paralelo * math.sin(angulo_rad))                                             
+                                                                                                     
+    rand_x = random.randint(-300, -lado_maior)                                     
+    rand_y = random.randint(altura, 300)           
 
     t.penup() # ou pu()
     t.goto(rand_x, rand_y)
@@ -115,19 +112,14 @@ def desenhar_trapezio(fill_color: str, lado_menor:int, lado_maior:int, lado_nao_
     t.end_fill()
 
 
-
 desenhar_plano_cartesiano()
 color = textinput("Insira uma cor","Digite a cor desejada no preenchimento: ")
-rand_x = random.randint(0,int(300))
-rand_y = random.randint(0,int(300))
-desenhar_quadrado(rand_x,rand_y,color,100)
+desenhar_quadrado(color,100)
 color = textinput("Insira uma cor","Digite a cor desejada no preenchimento: ")
 desenhar_triangulo_equilatero(color,100)
 color = textinput("Insira uma cor","Digite a cor desejada no preenchimento: ")
 desenhar_octogono(color,70)
 color = textinput("Insira uma cor","Digite a cor desejada no preenchimento: ")
 desenhar_trapezio(color, 50,100,10)
-
-
 
 mainloop()
