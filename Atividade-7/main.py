bff204cc4649399e1d6e977b30793dea6de727ee
vitorllr <vitorlopeslrocha@gmail.com>
2026-04-27 12:21:07 -0300

    forca minimamente funcional

--- Atividade-7/main.py
+++ Atividade-7/main.py
@@ -1,11 +1,16 @@
 import random 
+import unicodedata
 
 def pedir_letra():
-    letra = input("Digite a letra da palavra ou a palavra inteira: ").lower()
+    letra = input("Digite a letra da palavra ou a palavra inteira: ")
     if isinstance(letra, str) == False:
         print("Não podem ser escolhidos valores que nao strings")
-        letra = input("Digite a letra da palavra ou a palavra inteira: ").lower()
+        letra = input("Digite a letra da palavra ou a palavra inteira: ")
     return letra
+def normaliza_palavra_aleatoria(palavra):
+    for i, letra in enumerate(palavra):
+        normal = unicodedata.normalize('NFKD', letra)
+        palavra[i] = normal
 
 esportes =[
   "Futebol", "Basquete", "Vôlei", "Natação", "Tênis",
@@ -16,23 +21,35 @@ esportes =[
 
 vidas = 6
 tentativas = 0
-palavra_aleatoria = esportes[random.randint(0,len(esportes))]
-aparicao = 0
+palavra_aleatoria = esportes[random.randint(0,len(esportes))].lower()
 forca = len(palavra_aleatoria)*["_"]
-print(f"{forca}")
-print(f"{palavra_aleatoria}")
 
-while vidas > 0:
-   palpite = pedir_letra()
-   tentativas += 1
-   for i, letra in enumerate(palpite):
+print(f"{forca}")
+print(f"Palavra: {' '.join(forca)}")
+
+while vidas > 0 and "_" in forca:
+    palpite = pedir_letra().lower()
+    tentativas += 1
+
     if palpite == palavra_aleatoria:
-        print(f"Você acertou a palavra em {tentativas} !")
+        forca = list(palavra_aleatoria)
         break
-    elif letra in palavra_aleatoria:
-        aparicao += 1
-        forca[i] = letra
-        print(f"A letra escrita aparece {aparicao} vezes")
+
+
+    if palpite in palavra_aleatoria:
+        print(f"Boa! A letra '{palpite}' existe na palavra.")
+        for i, letra in enumerate(palavra_aleatoria):
+            if letra == palpite:
+                forca[i] = palpite
         print(f"{forca}")
     else:
-       vidas -= 1
+        vidas -= 1
+        print(f"Errou! Vidas restantes: {vidas}")
+
+
+if "_" not in forca:
+    print(f"Parabéns! Você acertou a palavra '{palavra_aleatoria}' em {tentativas} tentativas!")
+else:
+    print(f"Que pena! Você perdeu. A palavra era: {palavra_aleatoria}")
+    print(f"Progresso: {' '.join(forca)}")
+  
