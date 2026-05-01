9bdfb474c26ca5a50fdc2e93321da4e3cbd5ba45
vitor-rocha-quality <vitorlopeslrocha@gmail.com>
2026-05-01 14:46:04 -0300

    forca completa com modo palavra e modo letra

--- Atividade-7/main.py
+++ Atividade-7/main.py
@@ -1,54 +1,4 @@
-# import random 
 
-# def pedir_letra():
-#     letra = input("Digite a letra da palavra ou a palavra inteira: ")
-#     if isinstance(letra, str) == False:
-#         print("Não podem ser escolhidos valores que nao strings")
-#         letra = input("Digite a letra da palavra ou a palavra inteira: ")
-#     return letra
-
-
-# esportes =[
-#   "Futebol", "Basquete", "Volei", "Nataçao", "Tenis",
-#   "Atletismo", "Boxe", "Ciclismo", "Skate", "Surf",
-#   "Ginastica", "Rugby", "Handebol", "Medalha", "Treinador",
-#   "Estadio", "Arbitro", "Campeonato", "Corrida", "Olimpiadas"
-# ]
-
-# vidas = 6
-# tentativas = 0
-# palavra_aleatoria = esportes[random.randint(0,len(esportes))].lower()
-# forca = len(palavra_aleatoria)*["_"]
-
-# print(f"{forca}")
-# print(f"Palavra: {' '.join(forca)}")
-
-# while vidas > 0 and "_" in forca:
-#     palpite = pedir_letra().lower()
-#     tentativas += 1
-
-#     if palpite == palavra_aleatoria:
-#         forca = list(palavra_aleatoria)
-#         break
-
-
-#     if palpite in palavra_aleatoria:
-#         print(f"Boa! A letra '{palpite}' existe na palavra.")
-#         for i, letra in enumerate(palavra_aleatoria):
-#             if letra == palpite:
-#                 forca[i] = palpite
-#         print(f"{forca}")
-#     else:
-#         vidas -= 1
-#         print(f"Errou! Vidas restantes: {vidas}")
-
-
-# if "_" not in forca:
-#     print(f"Parabéns! Você acertou a palavra '{palavra_aleatoria}' em {tentativas} tentativas!")
-# else:
-#     print(f"Que pena! Você perdeu. A palavra era: {palavra_aleatoria}")
-#     print(f"Progresso: {' '.join(forca)}")
-  
 import random 
 import pygame
 from pygame import key
@@ -88,8 +38,9 @@ tentativas = 0
 palavra_aleatoria = esportes[random.randint(0,len(esportes))].lower()
 forca = len(palavra_aleatoria)*["_"]
 letras_tentadas = []
-mensagem = "Adivinhe a palavra!"
 modo = "letra"
+mensagem = f"Adivinhe a palavra!"
+chute = []
 
 running = True
 game_over = False
@@ -106,8 +57,8 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if modo == 'letra':
                 modo = 'palavra'
-            if modo == 'palavra':
-                modo = 'letra' 
+            elif modo == 'palavra':
+                modo = 'letra'
         
         if event.type == pygame.KEYDOWN and not game_over and modo == 'letra':
             letra = event.unicode.lower()
@@ -131,23 +82,39 @@ while running:
                 elif vidas <= 0:
                     mensagem = f"Que pena! A palavra era: {palavra_aleatoria}"
                     game_over = True
-        else:
-            letra = input("Digite a palavra inteira: ")
+                    
+        elif event.type == pygame.KEYDOWN and not game_over and modo == 'palavra':
+        
+            palpite = event.unicode.lower()
+            if palpite.isalpha() and len(palpite) == 1:
+                chute.append(palpite)
+                
+            if event.key == pygame.K_RETURN :
+                if "".join(chute) == palavra_aleatoria:
+                    for letra in chute: 
+                        letras_tentadas.append(letra)
+                    tentativas += 1
+                    mensagem = f"Parabéns! Venceu em {tentativas} tentativas! A palavra e '{palavra_aleatoria}'"
+                    game_over = True
+                else:
+                    vidas -= 1
+                    tentativas += 1
+                    mensagem = f"Errou! '{"".join(chute)}' não e a palavra"
+                    chute = []
+                
+
             
-            if letra == palavra_aleatoria:
-                letras_tentadas.append(letra)
-                tentativas += 1
-                mensagem = f"Parabéns! Venceu em {tentativas} tentativas!"
-                game_over = True
 
     # 4. Desenhar na Tela (Saída)
     palavra_display = " ".join(forca)
     texto_palavra = fonte.render(palavra_display, True, 'black')
     texto_mensagem = fonte_pequena.render(mensagem, True, 'blue')
+    texto_modo = fonte_pequena.render(f"modo: {modo}",True, 'green')
     texto_vidas = fonte_pequena.render(f"Vidas: {vidas}", True, (255, 0, 0))
     
     screen.blit(texto_palavra, (screen.get_width()//2 - texto_palavra.get_width()//2, 200))
     screen.blit(texto_mensagem, (screen.get_width()//2 - texto_mensagem.get_width()//2, 300))
+    screen.blit(texto_modo, (screen.get_width()//2 - texto_mensagem.get_width()//2, 350))
     screen.blit(texto_vidas, (50, 50))
     
 
