9e445cbd39c3425d5a957482bea2c5fc7f5fee2c
vitor-rocha-quality <vitorlopeslrocha@gmail.com>
2026-05-04 19:27:45 -0300

    mudanca de nome

--- Atividade-7/ppt/main.py
+++ Atividade-7/ppt/main.py
@@ -4,7 +4,7 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Jogo da Forca")
+pygame.display.set_caption("Pedra, Papel e Tesoura")
 fonte = pygame.font.SysFont("Arial", 40)
 fonte_pequena = pygame.font.SysFont("Arial", 30)
 clock = pygame.time.Clock()
