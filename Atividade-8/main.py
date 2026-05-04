dbca9f003d64a87cb695de3a3baeb9717c35b5a6
vitor-rocha-quality <vitorlopeslrocha@gmail.com>
2026-05-04 18:46:51 -0300

    versao inicial criptografia

--- Atividade-8/main.py
+++ Atividade-8/main.py
@@ -1,56 +1,73 @@
-
 def valida_dominio_email(email):
     return email[-8:] == "@puc.com"
+
 
 def possui_maiuscula(palavra):
     for c in palavra:
         if c.isupper():
             return True
     return False
+
+
 def possui_minuscula(palavra):
     for c in palavra:
         if c.islower():
             return True
     return False
 
+
 def possui_numero(palavra):
     for c in palavra:
         if c.isdigit():
-            return True  
+            return True
     return False
 
-     
+
 def avalia_senha(senha):
-    check_tamanho = len(senha) >= 8 
+    check_tamanho = len(senha) >= 8
     check_maiuscula = possui_maiuscula(senha)
     check_minuscula = possui_minuscula(senha)
     check_numero = possui_numero(senha)
 
     return check_tamanho and check_maiuscula and check_minuscula and check_numero
 
+
 def criptografa_a_senha(senha):
-    senha_criptografada = []
-    #pegar letra e converter para decimal ascii
+    senha_criptografada = ""
+    # pegar letra e converter para decimal ascii
+    for char in senha:
+        if char.isdigit():
+            # copiar logica do maiusculo trocando ref e o ciclo para 10
+            pass
+        elif "A" <= char <= "Z":
+            ref = ord("A")  # 65
+            ascii_char = ord(char)
+            pos_alfabeto = ascii_char - ref
+            pos_cesar = pos_alfabeto + 3
+            pos_cesar = pos_cesar % 26
+            letra_cifrada = chr(ref + pos_cesar)
+            senha_criptografada += letra_cifrada
+        elif "a" <= char <= "z":
+            pass
+        else:
+            senha_criptografada += char
+        return senha_criptografada
 
-    #pegar a diferenca entre 65 - c  
 
-    #somar 3 ao resulado acima ^
+# pegar a diferenca entre 65 - c
 
-    #obter o resto da divisao do resultado acima por 26
+# somar 3 ao resulado acima ^
 
-    #somar resultado acima a 65 e converter o valor para letra somando na lista
-    
-    # "".join(senha_criptografada)
+# obter o resto da divisao do resultado acima por 26
 
-    for c in senha:
-        if c.isalpha:
-            
-        senha_criptografada.append(c+3)
-    return senha_criptografada
+# somar resultado acima a 65 e converter o valor para letra somando na lista
 
-print(f"{valida_dominio_email("vitor@puc.com")}")
-print(f"{possui_maiuscula("abc@1234")}")
-print(f"{possui_minuscula("abc@1234")}")
-print(f"{possui_numero("abc@1234")}")
+# "".join(senha_criptografada)
 
-print(f"{avalia_senha("Senha1@aaaa")}")
+
+print(f"{valida_dominio_email('vitor@puc.com')}")
+print(f"{possui_maiuscula('abc@1234')}")
+print(f"{possui_minuscula('abc@1234')}")
+print(f"{possui_numero('abc@1234')}")
+
+print(f"{avalia_senha('Senha1@aaaa')}")
